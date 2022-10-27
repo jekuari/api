c471f4cd2176332a80f234cb348b719153461a64
Ricardo F <ricardoferegrinojr@gmail.com>
2022-10-27 01:35:30 -0500

    first cloud function

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const express = require('express');
+const app = express();
+const http = require('http');
+const https = require('https');
+
+
+app.route('/')
+      .get((req, res) => {
+         res.send('Hello World!');
+      });
+
+http.createServer(app).listen(80);
+https.createServer(app).listen(443);
